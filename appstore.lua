a7ec715b2577cc9afa1c076827d6950417a8b62e
nobleRadical <88314680+nobleRadical@users.noreply.github.com>
2024-01-14 02:33:01 -0800

    update appstore

--- appstore.lua
+++ appstore.lua
@@ -38,76 +38,74 @@ function checkVersion(file)
     end
 end
 
-command = arg[1]
-program = arg[2]
+program = arg[1]
 
 function main()
-if not fs.exists("apis/kasutils.lua") or program == "kasutils" then
+if not fs.exists("apis/kasutils.lua") then
     print"kasutils is required for most programs. Install?"
     local s = read()
     if s == "n" or s == "no" then
         print"suit yourself..."
     else
-        local utilFile = getFile("apis/kasutils.lua")
-        if utilFile then
-            Lfilewrite("apis/kasutils.lua", utilFile)
-        else
-            print"Could not connect to server."
-        end
+        installUtils()
     end
 end
-if command == "install" or command == "update" then
-    local remoteFile, reason = getFile(program .. ".lua")
-    if not remoteFile then
-        printError(reason)
-        return
-    end
-    local remoteVersion = checkVersion(remoteFile)
-    local localVersion = checkVersion(Lfileread(program .. ".lua"))
-    print("Remote version: v" .. tostring(remoteVersion))
 
-    if localVersion then
-        print("Local version: v" .. tostring(localVersion))
-    else
-        print("Not installed on your machine.")
-        
-    end
+if program == "kasutils" then
+    installUtils()
+    print "Installed."
+    return
+end
 
-    if not localVersion or remoteVersion > localVersion then
-        print(not localVersion and "Installing..." or "Updating...")
-        Lfilewrite(program .. ".lua", remoteFile)
-        print("Program updated.")
-    else
-        print("Program is already up-to-date.")
-    end
-elseif command == "check" then
-    local remoteFile, reason = getFile(program .. ".lua")
-    if not remoteFile then
-        printError(reason)
-        return
-    end
-    local remoteVersion = checkVersion(remoteFile)
-    local localVersion = checkVersion(Lfileread(program .. ".lua"))
-    print("Remote version: v" .. tostring(remoteVersion))
+local remoteFile, reason = getFile(program .. ".lua")
+if not remoteFile then
+    printError(reason)
+    return
+end
+local remoteVersion = checkVersion(remoteFile)
+local localVersion = checkVersion(Lfileread(program .. ".lua"))
+print("Remote version: v" .. tostring(remoteVersion))
 
-    if localVersion then
-        print("Local version: v" .. tostring(localVersion))
-    else
-        print("Not installed on your machine.")
-    end
+if localVersion then
+    print("Local version: v" .. tostring(localVersion))
 else
-    local progFile = getFile("programs.md")
-    if progFile then
-        print(progFile)
-    else
-        print "Cannot connect to server."
-        print("Code " .. tostring(HttpCode))
-        print("Reason: " .. tostring(HttpMessage))
-    end
-    printError[[Usage:
-appstore install|update program
-appstore check program]]
+    print("Not installed on your machine.")
+    
 end
+
+if not localVersion or remoteVersion > localVersion then
+    print(not localVersion and "Install? Y/N" or "Update? Y/N")
+    
+    Lfilewrite(program .. ".lua", remoteFile)
+    print("Program updated.")
+else
+    print("Program is already up-to-date.")
+end
+end
+
+function installUtils()
+    local utilFile, reason = getFile("apis/kasutils.lua")
+    if not utilFile then
+        printError(reason)
+        return
+    else
+        Lfilewrite("apis/kasutils.lua", utilFile)
+    end
+end
+
+function programs()
+    local progFile, reason = getFile("apis/kasutils.lua")
+    if not progFile then
+        printError(reason)
+        return
+    else
+        print(progFile)
+    end
+end
+
+function usage()
+    printError[[Usage:
+appstore <program>]]
 end
 
 main()
